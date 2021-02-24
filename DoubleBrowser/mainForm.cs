07cb606d6d26ffcfdc6b5d7f9d7accdb7d35c723
Wild Bill Custom <wildbl67@outlook.com>
2021-02-24 00:04:47 -0600

    First update

--- DoubleBrowser/mainForm.cs
+++ DoubleBrowser/mainForm.cs
@@ -23,58 +23,78 @@ namespace DoubleBrowser
             //this.treeViewLeft.NodeMouseClick += new TreeNodeMouseClickEventHandler(this.treeViewLeft_MouseClick);
         }
 
-        private void PopulateTreeView(string Epath, string INodeImage,TreeView mytreeview)
+       
+
+
+       
+
+        private void mainForm_Load(object sender, EventArgs e)
+
         {
+            
+            MyTrees treeClass = new MyTrees();
+
+            TreeNode lib = new TreeNode("My Computer", 0, 0);
+            TreeNode libright = new TreeNode("My Computer", 0, 0);
+
+            lib.ImageKey = "desktop";
+            libright.ImageKey = "desktop";
+            treeViewLeft.Nodes.Add(lib);
+            treeViewRight.Nodes.Add(libright);
             try
             {
-                TreeNode rootNode;
-                
-                //DirectoryInfo info = new DirectoryInfo(@"../..");
 
-                // Console.WriteLine("GetFolderPath: {5}", Environment.GetFolderPath(Environment.SpecialFolder.System));
-                DirectoryInfo info = new DirectoryInfo(@Epath);
-                if (info.Exists)
-                {
+                string fpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                //imageListIcons.Images.Add(Icon.ExtractAssociatedIcon(fpath));
+                string pPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string picPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                string cPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                //  string access = Directory.GetAccessControl(pPath).ToString();
+                //  Console.WriteLine("Path check " + pPath);
+                //  Console.WriteLine("Path Access" + access);
 
-                    rootNode = new TreeNode(info.Name, 1,1);
-                    rootNode.Tag = info;
-                    rootNode.ImageKey = INodeImage;
-                    GetDirectories(info.GetDirectories(), rootNode);
-                    mytreeview.Nodes.Add(rootNode);
-                }
+                DirectoryInfo md = new DirectoryInfo(pPath);
+
+                lib.Nodes.Add(fpath);
+                lib.Nodes.Add(md.Name);
+                lib.Nodes.Add(picPath);
+
+                libright.Nodes.Add(fpath);
+                libright.Nodes.Add(pPath);
+                libright.Nodes.Add(picPath);
+
+
+                treeClass.PopulateTreeView(fpath, "desktop", treeViewLeft);
+                treeClass.PopulateTreeView(pPath, "documents", treeViewLeft);
+                treeClass.PopulateTreeView(picPath, "documents", treeViewLeft);
+                treeClass.PopulateTreeView(cPath, "music", treeViewLeft);
+                treeClass.PopulateTreeView(fpath, "desktop", treeViewRight);
+                treeClass.PopulateTreeView(pPath, "documents", treeViewRight);
+                treeClass.PopulateTreeView(cPath, "music", treeViewRight);
+
+
+               treeClass.TreeViewDrives(treeViewLeft);
+
+                //treeClass.TreeViewDrives(treeViewRight);
+            }
+           
+            catch(System.NullReferenceException ex) { return; }
+            catch (Exception ex) {
+                string denideMessage = ex.Message;
+                if (denideMessage.Contains("denide access")) { return; } else { MessageBox.Show(ex.Message.ToString(), "Load exception" + ex.InnerException.ToString(), MessageBoxButtons.OK); }
+
             }
 
-            catch (IOException e) { MessageBox.Show(e.Message, "IO Exception" + e.Source.ToString(), MessageBoxButtons.OK); }
+           
+           
+            //PopulateTreeView(pPath);
+            //lib.Nodes.Add(PopulateTreeView(cPath,"music"));
 
-            catch (UnauthorizedAccessException e) { MessageBox.Show(e.Message, "UA Exception" + e.Source.ToString(), MessageBoxButtons.OK); }
 
-            catch (Exception e) { MessageBox.Show(e.Message.ToString(), e.Source.ToString(), MessageBoxButtons.OK); }
 
-        } //end Populatetreeviw
 
-        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
-        {
-            string tPath = "";
-            TreeNode aNode;
-            DirectoryInfo[] subSubDirs;
-            foreach (DirectoryInfo subDir in subDirs)
-            {
-                tPath = subDir.Name.ToString().Replace(@"\\",@"\");
 
-                aNode = new TreeNode(tPath, 0, 0);
-                aNode.Tag = subDir;
-                aNode.ImageKey = "folder";
-                subSubDirs = subDir.GetDirectories();
-                if (subSubDirs.Length != 0)
-                {
-                    GetDirectories(subSubDirs, aNode);
-                }
-                nodeToAddTo.Nodes.Add(aNode);
-            }
         }
-
-
-
         private void treeViewLeft_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
         {
             {
@@ -102,6 +122,7 @@ namespace DoubleBrowser
                     item.SubItems.AddRange(subItems);
                     listViewLeft.Items.Add(item);
                 }
+
                 foreach (FileInfo file in nodeDirInfo.GetFiles())
                 {
                     iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(file.FullName);
@@ -119,78 +140,9 @@ namespace DoubleBrowser
                 listViewLeft.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
             }
         }
-
-        private void mainForm_Load(object sender, EventArgs e)
-
-        {
-            TreeNode lib = new TreeNode("My Computer", 0, 0);
-            TreeNode libright = new TreeNode("My Computer", 0, 0);
-
-            lib.ImageKey = "desktop";
-            libright.ImageKey = "desktop";
-            treeViewLeft.Nodes.Add(lib);
-            treeViewRight.Nodes.Add(libright);
-            try
-            {
-
-                string fpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                string pPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                string cPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
-                //  string access = Directory.GetAccessControl(pPath).ToString();
-                //  Console.WriteLine("Path check " + pPath);
-                //  Console.WriteLine("Path Access" + access);
-
-
-
-                lib.Nodes.Add(fpath);
-                PopulateTreeView(fpath, "desktop",treeViewLeft);
-               PopulateTreeView(pPath, "documents",treeViewLeft);
-                PopulateTreeView(cPath, "music", treeViewLeft);
-                PopulateTreeView(fpath, "desktop", treeViewRight);
-                PopulateTreeView(pPath, "documents", treeViewRight);
-                PopulateTreeView(cPath, "music", treeViewRight);
-
-                //treeDrives(treeViewLeft);
-            }
-            catch (Exception ex) { MessageBox.Show(ex.Message.ToString(),"Load exception" + ex.InnerException.ToString(),MessageBoxButtons.OK); }
-            //PopulateTreeView(pPath);
-            //lib.Nodes.Add(PopulateTreeView(cPath,"music"));
-            
-
-
-
-
-        }
-
         /// <summary>
         /// 
         /// </summary>
-        private void treeDrives(TreeView mytree)
-        {
 
-
-
-
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
-            Console.WriteLine("Lenght d " + allDrives.Length);
-            foreach (DriveInfo d in allDrives)
-            {
-
-                /*string dPath = d.Name.ToString();
-                string access = Directory.GetAccessControl(dPath).ToString();
-                string dType = d.DriveType.ToString();*/
-                string path = @d.Name.ToString();
-                Console.WriteLine("Drive Name " + path);
-                Console.WriteLine("Drive type " + d.DriveType);
-
-
-                PopulateTreeView(d.Name.ToString(),"drive",mytree);
-
-
-
-
-            }
-
-        }
     }
 }
